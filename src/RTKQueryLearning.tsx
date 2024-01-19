1fc94e354a9ac082e881ded48beda6904a24cb88
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-01-19 20:01:45 -0300

    clean project

---
+++ src/RTKQueryLearning.tsx
@@ -0,0 +1,9 @@
+
+export const RTKQueryLearning = () => {
+  return (
+    <>
+    <h1>RTKQueryLearning</h1>
+    <hr />
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import { RTKQueryLearning } from './RTKQueryLearning'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RTKQueryLearning />
   </React.StrictMode>,
 )
